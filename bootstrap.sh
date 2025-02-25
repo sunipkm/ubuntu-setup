1bc22772807e9ca224018c532f30ae3a94b5680b
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2025-02-24 20:28:55 -0500

    Fix #1: Git config setup works correctly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
-SPWD=$(pwd)
-USER=$(whoami)
-
-if ! which git &> /dev/null; then
-    sudo apt update && sudo apt install git git-lfs
-fi
-
-# update git name and email
-GITEMAIL=`git config --global user.email`
-if [ ! "$GITEMAIL" ]; then
-    echo -n "Enter email for git: "
-    input=
-    while [[ $input = "" ]]; do
-    read input
-    done
-    git config --global user.email "$input"
-fi
-
-GITUSER=`git config --global user.name`
-if [ ! "$GITUSER" ]; then
-    echo -n "Enter user name (in full) for git: "
-    input=
-    while [[ $input = "" ]]; do
-    read input
-    done
-    git config --global user.name "$input"
-fi
-
 # the directory of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -48,5 +20,40 @@ function cleanup {
 # register the cleanup function to be called on the EXIT signal
 trap cleanup EXIT
 
+SPWD=$(pwd)
+USER=$(whoami)
+
+if ! which git &> /dev/null; then
+    sudo apt update && sudo apt install git git-lfs
+fi
+
+# update git name and email
+GITEMAIL=$(git config --global user.email)
+ret=$?
+if [ $ret != 0 ]; then
+    echo -n "Enter email for git: "
+    input=
+    while [[ $input = "" ]]; do
+    read input
+    done
+    git config --global user.email "$input"
+else
+    echo "Git global email set to: $GITEMAIL"
+fi
+
+GITUSER=$(git config --global user.name)
+ret=$?
+if [ $ret != 0 ]; then
+    echo -n "Enter user name (in full) for git: "
+    input=
+    while [[ $input = "" ]]; do
+    read input
+    done
+    git config --global user.name "$input"
+else
+    echo "Git global username set to: $GITUSER"
+fi
+
 cd $WORK_DIR && git clone https://github.com/sunipkm/ubuntu-setup && cd ubuntu-setup
 bash move_config.sh
+
