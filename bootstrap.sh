f6b53d4a1a67ce9a36c2d4d6904a8efc1ff9ee46
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2025-07-15 19:00:40 -0400

    Typst now installs to /usr/local

--- bootstrap.sh
+++ bootstrap.sh
@@ -466,7 +466,7 @@ fi
 
 if ! which typst &>/dev/null; then
     if [ "$RUST_INSTALLED" = true ]; then
-        confirm "Install Typst" && cargo install --locked typst-cli --root /usr/local/bin
+        confirm "Install Typst" && cargo install --locked typst-cli --root /usr/local
     else
         warn "Typst requires Rust, install the Rust toolchain, then run 'cargo install --locked typst-cli' to install Typst."
     fi
